183cdb571d7a9753443cd75695ab55639255b146
Stewart Lynch <slynch@createchsol.com>
2020-11-13 15:34:47 -0800

    Mods
    Modification on OnboardData to make instance properties more recognizable.

--- Onboarding/OnboardData.swift
+++ Onboarding/OnboardData.swift
@@ -11,11 +11,21 @@ enum OnboardData {
     static func buildSet(width:CGFloat = 350, height: CGFloat = 350) -> OnboardSet {
         let onboardSet = OnboardSet()
         onboardSet.dimensions(width: width, height: height)
-        onboardSet.newCard(title: "Login", image: "Login", text: "Enter your credentials and log in.")
-        onboardSet.newCard(title: "Update Profile", image: "Profile", text: "Make sure you update your profile and avatar.")
-        onboardSet.newCard(title: "Participate", image: "Engage", text: "Engage with others online.  Join the community.")
-        onboardSet.newCard(title: "Leave Feedback", image: "LeaveFeedback", text: "We want to hear from you so please let us know what you think.")
-        onboardSet.newCard(title: "Your Data", image: "Analytics", text: "Your data is your own.  View your stats at any time.")
+        onboardSet.newCard(title: "Login",
+                           image: "Login",
+                           text: "Enter your credentials and log in.")
+        onboardSet.newCard(title: "Update Profile",
+                           image: "Profile",
+                           text: "Make sure you update your profile and avatar.")
+        onboardSet.newCard(title: "Participate",
+                           image: "Engage",
+                           text: "Engage with others online.  Join the community.")
+        onboardSet.newCard(title: "Leave Feedback",
+                           image: "LeaveFeedback",
+                           text: "We want to hear from you so please let us know what you think.")
+        onboardSet.newCard(title: "Your Data",
+                           image: "Analytics",
+                           text: "Your data is your own.  View your stats at any time.")
         return onboardSet
     }
 }
--- Onboarding/OnboardSet.swift
+++ Onboarding/OnboardSet.swift
@@ -25,11 +25,21 @@ class OnboardSet {
 extension OnboardSet {
     static func previewSet() -> OnboardSet {
         let onboardSet = OnboardSet()
-        onboardSet.newCard(title: "Login", image: "Login", text: "Enter your credentials and log in.")
-        onboardSet.newCard(title: "Update Profile", image: "Profile", text: "Make sure you update your profile and avatar.")
-        onboardSet.newCard(title: "Participate", image: "Engage", text: "Engage with others online.  Join the community.")
-        onboardSet.newCard(title: "Leave Feedback", image: "LeaveFeedback", text: "We want to hear from you so please let us know what you think.")
-        onboardSet.newCard(title: "Your Data", image: "Analytics", text: "Your data is your own.  View your stats at any time.")
+        onboardSet.newCard(title: "Login",
+                           image: "Login",
+                           text: "Enter your credentials and log in.")
+        onboardSet.newCard(title: "Update Profile",
+                           image: "Profile",
+                           text: "Make sure you update your profile and avatar.")
+        onboardSet.newCard(title: "Participate",
+                           image: "Engage",
+                           text: "Engage with others online.  Join the community.")
+        onboardSet.newCard(title: "Leave Feedback",
+                           image: "LeaveFeedback",
+                           text: "We want to hear from you so please let us know what you think.")
+        onboardSet.newCard(title: "Your Data",
+                           image: "Analytics",
+                           text: "Your data is your own.  View your stats at any time.")
         return onboardSet
     }
 }
